a55491f9fab9ba83df1c5bf51482d8adea46edab
001001111001001 <gabijaques13@gmail.com>
2019-10-21 22:36:20 -0300

    inicio do projeto

---
+++ atividadegit/atividadegit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividadegit
+{
+    class Program
+    {
+        public static int somar(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+    }
+}
